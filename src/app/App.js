18d2b4b14f1fa296fdd8682801e1cac8a412594e
Gregory Wilson <gwilson@baosystems.com>
2016-11-17 16:05:18 -0500

    Removed sqlViews as they are unnecessary now. Added ability to disable accounts. Added hide-disabled accounts. Added search for user. Basic filtering works.

--- src/app/App.js
+++ src/app/App.js
@@ -17,7 +17,10 @@ import '../translationRegistration';
 
 const HeaderBar = withStateFrom(headerBarStore$, HeaderBarComponent);
 
+let injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
+//import Installer  from './Install.component.js';
 import Listing  from './Listing.component.js';
 //import Usage    from './Usage.component.js';
 //import Activity from './Activity.component.js';
@@ -90,6 +93,10 @@ export default React.createClass({
         //   return (<Usage d2={d2} />);
         //   break;
 
+        // case "install":
+        //   return (<Installer d2={d2} />);
+        //   break;
+
         //Default page
         default:
           return (<Listing d2={d2} />);
@@ -99,7 +106,8 @@ export default React.createClass({
     render() {
       const d2 = this.props.d2;
       const sections = [
-          { key: 'listing', icon:'people_outline',    label:d2.i18n.getTranslation('app_listing'), },
+        { key: 'listing', icon:'people_outline',    label:d2.i18n.getTranslation('app_listing'), },
+//        { key: 'install', icon:'perm_data_setting',    label:d2.i18n.getTranslation('app_install'), },
 //          { key: 'activity',icon:'person_pin',        label:d2.i18n.getTranslation('app_activity'), },
 //          { key: 'usage',   icon:'track_changes',     label:d2.i18n.getTranslation('app_usage'), },
       ].map(section => ({
--- src/app/Listing.component.js
+++ src/app/Listing.component.js
@@ -18,38 +18,12 @@ import Checkbox from 'material-ui/lib/checkbox';
 import TextField from 'material-ui/lib/text-field';
 import TextFieldLabel from 'material-ui/lib/text-field';
 import Slider from 'material-ui/lib/slider';
+import {green500, red500} from 'material-ui/lib/styles/colors';
 
 import AppTheme from '../colortheme';
 import HelpDialog from './HelpDialog.component';
 import actions from '../actions';
 
-
-/**
-sqlViews needed:
-
-//app-AccountAuditTracking-stale
-//Find logins later than a certain date. This mostly replicates existing DHIS2 functionality, but lets us see it in a prettier app interface.
-//No cache
-//sql query
-select u.userid, u.username, u.lastlogin, u.disabled, u.created, u.passwordlastupdated, u.creatoruserid,
-  ui.uid, ui.surname, ui.firstname, ui.email from users u
-join userinfo ui on u.userid=ui.userinfoid
-where u.lastlogin <= '${lastLoginDate}'
-order by u.lastlogin
-
-//app-AccountAuditTracking-fresh
-//Finds accounts that have logged in recently.
-//No cache
-//sql query
-select u.userid, u.username, u.lastlogin, u.disabled, u.created, u.passwordlastupdated, u.creatoruserid,
-  ui.uid, ui.surname, ui.firstname, ui.email from users u
-join userinfo ui on u.userid=ui.userinfoid
-where u.lastlogin >= '${lastLoginDate}'
-order by u.lastlogin
-
-
-
-*/
 const help = {
   help:(
     <div>
@@ -88,105 +62,206 @@ export default React.createClass({
       const d2 = this.props.d2;
       const api = d2.Api.getApi();
 
-      api.get('sqlViews').then(promise=>{
-        let fresh = false;
-        let stale = false;
-        if (promise.hasOwnProperty('sqlViews')){
-          let sqv = promise.sqlViews;
-          for (let view of sqv){
+      //get OU tree
+      d2.models.organisationUnits.list({ paging: false, level: 1, fields: 'id,displayName,children::isNotEmpty' })
+          .then(rootLevel => rootLevel.toArray()[0])
+          .then(rootUnit => {
+            this.setState({ouRoot:rootUnit});
+          })
+          .catch(err => console.log(err));
 
-            if (view.displayName==='app-AccountAuditTracking-fresh'){
-              fresh = view.id;
-            }
-            if (view.displayName==='app-AccountAuditTracking-stale'){
-              stale = view.id;
-            }
-          }
+      //get user groups for filtering
+      api.get('/userGroups?paging=false').then(promise=>{
+        if (promise.hasOwnProperty('userGroups')){
+          this.setState({
+            userGroups:promise.userGroups,
+          });
         }
-
-        if (fresh === false){
-          actions.showSnackbarMessage('Cannot find "app-AccountAuditTracking-fresh" sqlView. Click the Help button.');
-          this.setState({errors:'Cannot find "app-AccountAuditTracking-fresh" sqlView'});
+        else{
+          this.setState({
+            userGroups:[],
+          })
         }
-        if (stale === false){
-          actions.showSnackbarMessage('Cannot find "app-AccountAuditTracking-stale" sqlView. Click the Help button.');
-          this.setState({errors:'Cannot find "app-AccountAuditTracking-stale" sqlView'});
-        }
-        this.setState({sqlView_fresh_ID:fresh,sqlView_stale_ID:stale});
       })
-      .catch(error=> {
-        actions.showSnackbarMessage('Cannot find necessary sqlViews. Click the Help button.');
-        this.setState({errors:'Cannot find necessary sqlViews'});
-      });
+      .catch(err => console.log(err));
 
+      //get the base URL for the DHIS install
+      let href = window.location.href;
+      //on a app the system path starts with /api so chop off everything from there
+      if (href.indexOf('/api/')!==-1){
+        href = href.slice(0,href.indexOf('/api/'));
+      }
+      this.setState({urlPath:href});
     },
 
     getInitialState() {
         return {
-          type:'stale',         // stale (longer than X days), fresh (within past X days)
-          days:90,          // # days
+          type:'stale',       // stale (longer than X days), fresh (within past X days)
+          days:90,            // # days
+          data:[],            // resulting user list
           filterBy:'none',    // none, group, ou
-          invertFilter:false, // select everyone not in the given params
-          data:[],
+          filterUsername:'',  // for finding a particular person
+          filter:null,
+          filterDisabled:false, // hide disabled users
+//          invertFilter:false, // select everyone not in the given params
+//          sqlViews:[],        // cache of sqlViews IDs
+          ouRoot:null,        // top of the OU tree, needed for OrgUnitTree
+          userGroups:[],      // all user groups, needed for filter
+          urlPath:'',         // server path, needed for user edit link
           processing:false,
           errors:'',
-          sqlView_fresh_ID:false,
-          sqlView_stale_ID:false,
         };
     },
 
+    //switch the type of search
     handleTypeChange(event, index, value){
       this.setState({type:value});
     },
 
+    //update how they want to filter the data
     handleFilterChange(event, index, value){
-      this.setState({filterBy:value});
+      this.setState({filter:null,filterBy:value});
     },
 
+    //they want to look up a particular person
+    handleUserChange(event){
+      this.setState({filterUsername:event.target.value});
+    },
+
+    //they changed the number of days
     handleLengthChange(event, index, value){
       this.setState({days:index});
     },
 
-    handleInvert(event, value){
-      this.setState({invertFilter:value});
+    handleFilterDisabled(event, value){
+      this.setState({filterDisabled:value});
       console.log(value);
     },
 
-    getOUTree(){
-      console.log('hi')
+    //Clicking on the org tree
+    handleSelectedOrgUnit(event, model) {
+      this.setState({
+          filter: (model.id === this.state.filter)?null:model.id,
+      });
     },
 
-    process(){
+    //Clicking on the org tree
+    handleGroupChange(event, index, value) {
+      this.setState({
+          filter: value,
+      });
+    },
 
+    //Toggling of user access
+    handleDisableUser(user) {
+      const d2 = this.props.d2;
+      const api = d2.Api.getApi();
+
+      let data = {
+        username:user.userCredentials.username,
+      };
+      if (user.userCredentials.disabled===true){  //toggle to enable
+        data.enable=true;
+      }
+      //send the enable/disable request
+      api.jquery
+        .ajax({
+          type: 'POST',
+          url: this.state.urlPath+'/dhis-web-maintenance-user/disableUser.action',
+          data: data
+        })
+        .done((res, textStatus, xhr)=>{
+          //Worked! Update the UI
+          if (xhr.status===200 && xhr.statusText==='OK'){
+            user.userCredentials.disabled = !user.userCredentials.disabled;
+            let users = this.state.data;
+            for (let i in users){
+              if (users[i].id === user.id){
+                console.log('found');
+                users[i].userCredentials.disabled = user.userCredentials.disabled;
+                actions.showSnackbarMessage("User ''"+user.userCredentials.username+"' updated'");
+                break;
+              }
+            }
+            this.setState({data:users});
+          }
+        })
+        .fail(err=>{
+          this.setState({errors:err});
+        })
+        .always(()=>{
+
+        });
+    },
+
+    //Show the OU tree if that is the current filter
+    getOUTree(){
+      if (this.state.filterBy === 'ou'){
+          return (
+            <OrgUnitTree
+              root={this.state.ouRoot}
+              onClick={this.handleSelectedOrgUnit}
+              selected={[this.state.filter]}
+              selectedLabelStyle={{fontWeight:'bold'}}
+            />
+          );
+      }
+      return null;
+    },
+
+    //Show the available User groups
+    getUserGroups(){
+      if (this.state.filterBy === 'group'){
+        let groups = [];
+        for (let i of this.state.userGroups){
+          groups.push(<MenuItem value={i.id} key={i.id} primaryText={i.displayName} />);
+        }
+        return (
+          <SelectField
+          floatingLabelText="Group"
+          value={this.state.filter}
+          onChange={this.handleGroupChange}
+        >
+          {groups}
+        </SelectField>
+        );
+      }
+      return null;
+    },
+
+    //The search button was pressed
+    process(){
       const d2 = this.props.d2;
       const api = d2.Api.getApi();
       this.setState({processing:true});
 
-      if (this.state.sqlView_fresh_ID === false || this.state.sqlView_stale_ID === false){
-        actions.showSnackbarMessage('Necessary sqlViews have not been not created. Aborting.');
-        return;
-      }
-
-      //figure out the lastLoginDate
+      //figure out the lastLoginDate as an actual Date
       var lastLoginDate = new Date();
       lastLoginDate.setDate(lastLoginDate.getDate() - this.state.days);
 
-      //generate the correct sqlview path
-      let route = 'sqlViews/';
-      switch (this.state.type){
-        case 'stale':
-          route = route + this.state.sqlView_stale_ID;
-        break;
-        case 'fresh':
-          route = route + this.state.sqlView_fresh_ID;
-        break;
+      let data = {
+        fields:'*',
+        pageSize:50,
+      };
+      if (this.state.type==='fresh'){
+        data.lastLogin=lastLoginDate.toISOString().substr(0,10);
       }
-      route = route + '/data.json?var=lastLoginDate:'+lastLoginDate.toISOString().substr(0,10);
+      else if (this.state.type==='stale'){
+        data.inactiveSince=lastLoginDate.toISOString().substr(0,10);
+      }
+      if (this.state.filterBy==='ou' && this.state.filter!==false){
+          data.ou=this.state.filter;
+      }
+      if (this.state.filterUsername!==''){
+          data.query=this.state.filterUsername;
+      }
 
-      api.get(route).then(promise=>{
-        if (promise.hasOwnProperty('rows')){
+      api.get('users',data).then(promise=>{
+        if (promise.hasOwnProperty('users')){
+          console.log(promise.users);
+
           this.setState({
-            data:promise.rows,
+            data:promise.users,
             processing:false,
           });
         }
@@ -206,25 +281,75 @@ export default React.createClass({
     render() {
         const d2 = this.props.d2;
 
+        let users = this.state.data.map( (row) => {
+          //Do some filtering...
+          if (this.state.filterBy==='group'){
+            let found = false;
+            for (let g of row.userGroups){
+              if (g.id === this.state.filter){
+                found = true;
+              }
+            }
+            if (found === false){
+              return null;
+            }
+          }
+          else if (this.state.filterBy==='ou'){
+            let found = false;
+            for (let g of row.organisationUnits){
+              if (g.id === this.state.filter){
+                found = true;
+              }
+            }
+            if (found === false){
+              return null;
+            }
+          }
+          //hide disabled records
+          if (this.state.filterDisabled===true && row.userCredentials.disabled===true){
+            return null;
+          }
+
+          //Filters done, display records
+          return (
+          <TableRow key={row.id} selectable={false}>
+            <TableRowColumn>{row.displayName}</TableRowColumn>
+            <TableRowColumn>{row.userCredentials.username}</TableRowColumn>
+            <TableRowColumn>{row.email}</TableRowColumn>
+            <TableRowColumn>
+              <IconButton id={row.id} tooltip={(row.userCredentials.disabled===false)?"Disable":"Enable"}
+                onTouchTap={this.handleDisableUser.bind(this,row)}>
+                {(row.userCredentials.disabled===false)?
+                  <FontIcon className="material-icons">check</FontIcon>:
+                  <FontIcon className="material-icons">block</FontIcon>
+                }
+              </IconButton>
+            </TableRowColumn>
+            <TableRowColumn>{row.userCredentials.lastLogin}</TableRowColumn><TableRowColumn>{row.userCredentials.passwordLastUpdated}</TableRowColumn>
+          </TableRow>
+        )});
+
+
         return (
             <div className="wrapper">
               <HelpDialog style={{float:"right"}} title={"App Help"} content={help.help} />
 
-              <Paper className='paper' style={{'minWidth':'600px'}}>
+              <Paper className='paper' style={{'minWidth':'800px'}}>
                 <h3 className="subdued title_description">{d2.i18n.getTranslation('app_listing')}</h3>
 
-                <div style={{'width':'50%','float':'left'}}>
+                <div style={{'width':'40%','float':'left'}}>
                   <SelectField  value={this.state.type}
                                 onChange={this.handleTypeChange}
-                                floatingLabelText='Find Users'
+                                floatingLabelText='Find Users who '
                                 maxHeight={200}
                                 style={{'float':'left'}}>
-                    <MenuItem value='fresh' key='fresh' label='Fresh' primaryText='Last logged in &lt; X' />
-                    <MenuItem value='stale' key='stale' label='Stale' primaryText='Last logged in &gt; X'/>
+                    <MenuItem value='fresh' key='fresh' label='Have' primaryText='Have' />
+                    <MenuItem value='stale' key='stale' label='Have Not' primaryText='Have Not'/>
                   </SelectField>
 
-                  <div style={{'float':'left','clear':'both','width':'10em'}} >
-                    <span style={{'float':'right'}}>{this.state.days} days ago</span>
+                  <div style={{'float':'left','clear':'both','width':'70%'}} >
+                    <span style={{'float':'left',fontSize:'small',color:'grey'}}>Logged in the past&nbsp;
+                      <span style={{fontSize:'normal',fontWeight:'bold',color:'black'}}>{this.state.days}</span> days</span>
                     <Slider
                         step={1}
                         value={this.state.days}
@@ -232,10 +357,17 @@ export default React.createClass({
                         min={1}
                         max={180}
                         onChange={this.handleLengthChange}
+                        style={{marginBottom:'0px'}}
                         />
+                      <TextField hintText="Name"
+                      floatingLabelText="With name containing"
+                      floatingLabelFixed={true}
+                      value={this.state.filterUsername}
+                      onChange={this.handleUserChange} />
                   </div>
                 </div>
-                <div style={{'width':'50%','float':'left'}}>
+
+                <div style={{'width':'40%','float':'left'}}>
                   <SelectField  value={this.state.filterBy}
                                 onChange={this.handleFilterChange}
                                 floatingLabelText='Filter By'
@@ -245,22 +377,19 @@ export default React.createClass({
                     <MenuItem value='group' key='group' primaryText='User Group' />
                     <MenuItem value='ou' key='ou' primaryText='Organizational Unit' />
                   </SelectField>
+                  <div style={{'clear':'both'}}>
+                    {this.getOUTree()}
+                    {this.getUserGroups()}
+                  </div>
+                </div>
 
-                  {this.getOUTree()}
-{/*
-                  <OrgUnitSelectAll
-                               root=
-                               selected={this.state.selected}
-                               initiallyExpanded={this.props.root.id}
-                               onClick={(e) => { e.target.parentNode.parentNode.firstChild.click(e); }}
-                           />
+                <div style={{'width':'19%','float':'left'}}>
 
-                  <p>[Filter Options Box]</p>
+                  <Checkbox label="Hide Disabled"
+                        checked={this.state.filterDisabled}
+                        onCheck={this.handleFilterDisabled}
+                        labelStyle={{color:'grey',fontSize:'small'}}/>
 
-                  <Checkbox label="Invert Filter"
-                        checked={this.state.invertFilter}
-                        onCheck={this.handleInvert} />
-                        */}
                 </div>
                 <RaisedButton
                   label="Search"
@@ -283,7 +412,7 @@ export default React.createClass({
                         Name
                       </TableHeaderColumn>
                       <TableHeaderColumn>
-                        ID
+                        UserName
                       </TableHeaderColumn>
                       <TableHeaderColumn>
                         Email
@@ -297,15 +426,6 @@ export default React.createClass({
                       <TableHeaderColumn>
                         Password Updated
                       </TableHeaderColumn>
-                      <TableHeaderColumn>
-                        Edit
-                      </TableHeaderColumn>
-                      <TableHeaderColumn>
-                        Disable
-                      </TableHeaderColumn>
-                      <TableHeaderColumn>
-                        Delete
-                      </TableHeaderColumn>
                     </TableRow>
                   </TableHeader>
                   <TableBody
@@ -313,35 +433,7 @@ export default React.createClass({
                             showRowHover={true}
                             stripedRows={true}
                           >
-
-
-                            {this.state.data.map( (row, index) => (
-                              <TableRow key={row[0]} selectable={false}>
-                                <TableRowColumn>{row[1]}</TableRowColumn>
-                                <TableRowColumn>{row[7]}</TableRowColumn>
-                                <TableRowColumn>{row[10]}</TableRowColumn>
-                                <TableRowColumn>
-                                  {(row[3]=="false")?<FontIcon className="material-icons">check</FontIcon>:null}
-                                </TableRowColumn>
-                                <TableRowColumn>{row[2]}</TableRowColumn>
-                                <TableRowColumn>{row[5]}</TableRowColumn>
-                                <TableRowColumn>
-                                  <IconButton tooltip="Font Icon">
-                                    <FontIcon className="material-icons">edit</FontIcon>
-                                  </IconButton>
-                                </TableRowColumn>
-                                <TableRowColumn>
-                                  <IconButton tooltip="Font Icon">
-                                    <FontIcon className="material-icons">block</FontIcon>
-                                  </IconButton>
-                                </TableRowColumn>
-                                <TableRowColumn>
-                                  <IconButton tooltip="Font Icon">
-                                    <FontIcon className="material-icons">delete</FontIcon>
-                                  </IconButton>
-                                </TableRowColumn>
-                              </TableRow>
-                              ))}
+                            {users}
                   </TableBody>
                 </Table>
               </Paper>
